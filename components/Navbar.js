661bf01cc60b0d79a615926874604b2c31832987
Bir <140426404+Tarunbir003@users.noreply.github.com>
2024-07-23 15:12:15 -0600

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-xl font-bold">
+          <Link href="/">IMR Company</Link>
+        </div>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/movies">
+              <a className="text-white hover:text-gray-400">Movies List</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/add-movie">
+              <a className="text-white hover:text-gray-400">Add Movie</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a className="text-white hover:text-gray-400">About Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a className="text-white hover:text-gray-400">Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
