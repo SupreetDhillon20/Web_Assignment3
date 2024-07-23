980e446ea170a7e392f2432ad4d2e236985a030f
Bir <140426404+Tarunbir003@users.noreply.github.com>
2024-07-23 15:12:58 -0600

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 p-4 text-center text-white">
+      <div className="container mx-auto">
+        <p>© 2023 Internet Movies Rental Company</p>
+        <p>Contact: support@imrcompany.com</p>
+        <p>123 Movie Lane, Filmtown, CA 90210</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
