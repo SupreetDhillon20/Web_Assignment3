a6534d8227ee1d4b167df2a543415d194772a9f1
SupreetDhillon20 <148576840+SupreetDhillon20@users.noreply.github.com>
2024-07-23 20:22:11 -0600

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,27 @@
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Link from 'next/link';
 
-const Layout = ({ children }) => {
+const Navbar = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow">{children}</main>
-      <Footer />
-    </div>
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-xl font-bold">
+          <Link href="/">IMR Company</Link>
+        </div>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/movies">
+              <span className="text-white hover:text-gray-400">Movies List</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <span className="text-white hover:text-gray-400">About Us</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
-export default Layout;
+export default Navbar;
