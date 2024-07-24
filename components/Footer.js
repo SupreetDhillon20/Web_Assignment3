652560ee4801aa05eeb934d83bf9a215993fe264
SupreetDhillon20 <148576840+SupreetDhillon20@users.noreply.github.com>
2024-07-23 20:21:40 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,34 @@
+import React from 'react';
 const Footer = () => {
-  return (
-    <footer className="bg-gray-800 p-4 text-center text-white">
-      <div className="container mx-auto">
-        <p>© 2023 Internet Movies Rental Company</p>
-        <p>Contact: support@imrcompany.com</p>
-        <p>123 Movie Lane, Filmtown, CA 90210</p>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+    return (
+      <footer className="bg-gray-800 p-4 text-center text-white">
+        <div className="container mx-auto">
+          <p>&copy; 2023 Internet Movies Rental Company</p>
+        </div>
+        <div className="mb-4 text-left">
+            <p>
+                <p className='font-bold'>Address:</p>
+                123 Movie Lane, Filmtown<br />
+                Calgary, AB, T1K 1L3
+            </p>
+        </div>
+        <div className="mb-4 text-left">
+            <p>
+                <p className='font-bold'>Contact Information:</p>
+                Phone: (338) 456-7890<br />
+                Email: <a href="mailto:support@imrcompany.com" className='text-white hover:text-blue-500'>support@imrcompany.com</a>
+            </p>
+        </div>
+        <div>
+            <p>
+                Follow Us:<br />
+                <a href="https://facebook.com/abchighschool" className="text-white hover:text-blue-500 ml-2">Facebook</a>, 
+                <a href="https://twitter.com/abchighschool" className="text-white hover:text-blue-500 ml-2">Twitter</a>, 
+                <a href="https://instagram.com/abchighschool" className="text-white hover:text-blue-500 ml-2">Instagram</a>
+            </p>
+        </div>
+      </footer>
+    );
+  };
+  
+  export default Footer;
